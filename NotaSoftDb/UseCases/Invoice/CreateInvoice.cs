ac381df25ca0ead4410fc63d0ff4f1bca89cf3d6
rafael <rafaondjango@gmail.com>
2024-03-20 12:45:42 -0300

    feat: criando use cases para a model invoice

---
+++ NotaSoftDb/UseCases/Invoice/CreateInvoice.cs
@@ -0,0 +1,17 @@
+﻿using NotaSoftDb.Models;
+using NotaSoftDb.Repositories;
+
+namespace NotaSoftDb.UseCases.InvoiceCases;
+
+public class CreateInvoice
+{
+	public static async Task Execute(Invoice invoice)
+	{
+		var dbContext = new CustomDbContext();
+		var invoiceRepository = new InvoiceRepository(dbContext);
+
+		invoiceRepository.Create(invoice);
+
+		await dbContext.SaveChangesAsync();
+	}
+}
