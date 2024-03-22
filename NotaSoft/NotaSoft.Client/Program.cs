5a1ba2d2210cf486418cea01a4c656559b99f38a
rafael <rafaondjango@gmail.com>
2024-03-21 23:48:47 -0300

    feat: configurando cors

--- NotaSoft/NotaSoft.Client/Program.cs
+++ NotaSoft/NotaSoft.Client/Program.cs
@@ -4,4 +4,10 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
 builder.Services.AddBlazorBootstrap();
 
+builder.Services.AddScoped(sp =>
+	new HttpClient
+	{
+		BaseAddress = new Uri("https://localhost:7184")
+	});
+
 await builder.Build().RunAsync();
--- NotaSoftApi/Program.cs
+++ NotaSoftApi/Program.cs
@@ -7,6 +7,20 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+builder.Services.AddCors(options =>
+{
+	options.AddPolicy(name: MyAllowSpecificOrigins,
+					  policy =>
+					  {
+						  policy.WithOrigins("https://localhost:7237",
+											  "http://localhost:5265")
+								.AllowAnyHeader()
+								.AllowAnyMethod();
+					  });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -17,6 +31,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors(MyAllowSpecificOrigins);
 
 app.UseAuthorization();
 
